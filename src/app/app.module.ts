efdc1efc38a6b9a3c2dc6f49041738a9619ae5ef
samorgill <orgillsams@gmail.com>
2018-03-17 14:51:06 +0000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe/recipe-book/recipe-book.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipe/recipe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ShoppingListEditComponent,
+    IngredientComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
