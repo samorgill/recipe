5053ed1f36325a96baf85ed4f443e2e50b26054c
samorgill <orgillsams@gmail.com>
2018-03-17 21:30:56 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipe/recipe-book/recipe-book.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
@@ -17,9 +15,7 @@ import { RecipeComponent } from './recipe/recipe.component';
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeBookComponent,
     ShoppingListEditComponent,
-    IngredientComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
