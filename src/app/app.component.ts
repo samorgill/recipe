bd63a5de4470cabf7d61638116f9c63c8a7903b4
samorgill <orgillsams@gmail.com>
2018-04-01 16:10:54 +0100

    Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  which = 'recipes';
+
+  selectApp(event: string){
+    this.which = event;
+  }
+
 }
