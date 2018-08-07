ee50fe394dd7dcc2ab7086a76f0f241200f79557
sam.orgill <sam.orgill@ultra-as.com>
2018-08-07 17:03:20 +0100

    shopping-list service

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,3 +1,27 @@
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {LogService} from './log.service';
+
+@Injectable()
 export class ShoppingListService {
 
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Basil', 5),
+    new Ingredient('Tomato', 1),
+  ];
+
+  constructor(private logService: LogService){}
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient){
+    this.logService.log(' in service');
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+
+  }
 }
